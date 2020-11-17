2cee61b17e69aa8ef01ecd703664e72b78731f52
ima9rd <ima9rd@gmail.com>
2020-11-17 14:48:46 -0600

    Lazy script for lazy people

---
+++ app.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+sel = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+sel.delete_all_cookies()
+sel.get('https://direct.playstation.com/en-us/ps5')
+
+queue = False
+while not queue:
+    body = sel.find_element_by_tag_name('body')
+    if body.get_attribute('class') == 'queue challenge':
+        queue = True
+    else:
+        time.sleep(10)
+        sel.refresh()
