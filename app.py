d8e7663ddbc8f32505f9bec32edea76838904d9e
ima9rd <ima9rd@gmail.com>
2020-11-17 15:03:09 -0600

    Added obnoxious beeping sound

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from random import randint
 import time
+import winsound
+
+
+def beep(repeat):
+    for i in range(repeat):
+        winsound.Beep(400, 500)
+        time.sleep(1)
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -15,6 +22,7 @@ while not queue:
     body = sel.find_element_by_tag_name('body')
     if body.get_attribute('class') == 'queue challenge':
         queue = True
+        beep(5)
     else:
         time.sleep(randint(4, 10))
         sel.refresh()
