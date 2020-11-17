20131930dd5060dc7342c9f16804418b24bac192
danielkalen <d@danielkalen.com>
2020-11-17 17:58:04 -0500

    add support for unix; various improvements

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from selenium import webdriver
+from selenium import webdriver, common
 from webdriver_manager.chrome import ChromeDriverManager
 from random import randint
 import time
-import winsound
+import pyttsx3
 
+CAPTCHA_TIME_ALLOWANCE = 30
 
-def beep(repeat):
+speakengine = pyttsx3.init()
+
+def say(message, repeat=1):
     for i in range(repeat):
-        winsound.Beep(400, 500)
-        time.sleep(1)
+        speakengine.say(message)
+        speakengine.runAndWait()
+
+def schedule_refresh():
+    time.sleep(randint(4, 10))
+    sel.refresh()
+    print('refreshing')
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -22,7 +30,17 @@ while not queue:
     body = sel.find_element_by_tag_name('body')
     if body.get_attribute('class') == 'queue challenge':
         queue = True
-        beep(5)
+        say("ps5 queue is active")
+    if body.get_attribute('class') == 'softblock':
+        say("captcha-challenge block")
+        time.sleep(CAPTCHA_TIME_ALLOWANCE) # allow 20 seconds to solve captcha before attempting to refresh
     else:
-        time.sleep(randint(4, 10))
-        sel.refresh()
+        try:
+            add_to_cart_button = sel.find_element_by_class_name('add-to-cart') # first add-to-cart button
+            if "hide" not in add_to_cart_button.get_attribute('class'):
+                say("ps5 is potentially available for purchase!", 3)
+            else:
+                schedule_refresh()
+        
+        except common.exceptions.NoSuchElementException:
+            schedule_refresh()
